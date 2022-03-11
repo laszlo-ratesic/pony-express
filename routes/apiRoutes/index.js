edec3edd10a832b118b2433690e5a5fb3e4e38e2
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-11 08:47:31 -0600

    re-route sendmail routes to api routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { v4 } = require('uuid');
+const multiparty = require('multiparty');
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+
+router.get('/', (req, res) => {
+    const path = `/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+router.get('/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+});
+
+// Create transport object
+const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  // Verify connection configuration
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
+router.post("/send", (req, res) => {
+    // accept formdata and parse w/ multiparty
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+      console.log(fields);
+      Object.keys(fields).forEach(function (property) {
+        data[property] = fields[property].toString();
+      });
+
+      console.log(`<${data.email}>`);
+      // Create mail object w/ fields
+      const mail = {
+        from: `"P.T.H. Website Admin" <${process.env.EMAIL}>`,
+        to: `${process.env.EMAIL}, info@postcardsthroughhell.com`,
+        subject: "[contact-message]: Postcards Through Hell",
+        html: `From: ${data.name} | ${data.company}<br/>
+        Referred by: ${data.referral}<br/>
+        <${data.email}><br/>
+        ${data.message}<br/>
+        <br/>
+        <a href="mailto:${data.email}">Reply to ${data.name}'s email - ${data.email}</a>`,
+      };
+
+      // Use transporter to send mail
+      transporter.sendMail(mail, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Something went wrong");
+        } else {
+          res.status(200).send("Email successfully sent to recipient!");
+        }
+      });
+    });
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
-const nodemailer = require("nodemailer");
-const multiparty = require("multiparty");
-const { transformSync } = require("@babel/core");
-require("dotenv").config();
+const apiRoutes = require("./routes/apiRoutes");
 
 // Testing port will be 5000
 const PORT = process.env.PORT || 5000;
@@ -17,62 +14,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.use("/", htmlRoutes);
-
-// Create transport object
-const transporter = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com",
-  port: 587,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
-
-// Verify connection configuration
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
-
-// POST route
-app.post("/send", (req, res) => {
-  // accept formdata and parse w/ multiparty
-  let form = new multiparty.Form();
-  let data = {};
-  form.parse(req, function (err, fields) {
-    console.log(fields);
-    Object.keys(fields).forEach(function (property) {
-      data[property] = fields[property].toString();
-    });
-
-    console.log(`<${data.email}>`);
-    // Create mail object w/ fields
-    const mail = {
-      from: `"P.T.H. Website Admin" <${process.env.EMAIL}>`,
-      to: `${process.env.EMAIL}, info@postcardsthroughhell.com`,
-      subject: "[contact-message]: Postcards Through Hell",
-      html: `From: ${data.name} | ${data.company}<br/>
-      Referred by: ${data.referral}<br/>
-      <${data.email}><br/>
-      ${data.message}<br/>
-      <br/>
-      <a href="mailto:${data.email}">Reply to ${data.name}'s email - ${data.email}</a>`,
-    };
-
-    // Use transporter to send mail
-    transporter.sendMail(mail, (err, data) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Something went wrong");
-      } else {
-        res.status(200).send("Email successfully sent to recipient!");
-      }
-    });
-  });
-});
+app.use("/api", apiRoutes);
 
 // EXPRESS SERVER LISTENING
 app.listen(PORT, () => {
